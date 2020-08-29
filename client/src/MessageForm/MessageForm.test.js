2913448973b06765c4bbab4f21c9f43f93bd7e08
villedarony91 <villedarony91>
2020-08-29 17:04:28 -0600

    Add tests to render all required components

---
+++ client/src/MessageForm/MessageForm.test.js
@@ -0,0 +1,15 @@
+import { configure, shallow  } from "enzyme";
+import Adapter from "enzyme-adapter-react-16";
+import MessageForm from "./MessageForm";
+import React from 'react';
+
+configure({ adapter: new Adapter() });
+
+describe("Test Message form", () => {
+  it("should render all necessary input controls", () => {
+      const wrapper = shallow(<MessageForm></MessageForm>);
+      expect(wrapper.find('#submit')).toHaveLength(1);
+      expect(wrapper.find('#top-label')).toHaveLength(1);
+      expect(wrapper.find('#message-to-send')).toHaveLength(1);
+  });
+});
